8d079df9426194c1860d8d2cb2e20e4ef2739787
johnoro <officialjohnorourke@gmail.com>
2019-03-20 17:48:37 -0400

    Adds express routing with examples

---
+++ api/users/register.js
@@ -0,0 +1,7 @@
+const router = require('express').Router();
+
+router.post('', (req, res) => {
+  // call the user table and insert into the request body into it; might need some validation
+});
+
+module.exports = router;
