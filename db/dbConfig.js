7a809f92909089e74067f07e966e58f6d69a25fd
Grant Reighard <grant.reighard@gmail.com>
2019-03-20 17:18:27 -0400

    Updates dbConfig to use environment variable.

--- db/dbConfig.js
+++ db/dbConfig.js
@@ -1,4 +1,5 @@
 const knex = require('knex');
 const knexConfig = require('../knexfile');
 
-module.exports = knex(knexConfig.development);
+const env = process.env.DB_ENV || 'development';
+module.exports = knex(knexConfig[env]);
