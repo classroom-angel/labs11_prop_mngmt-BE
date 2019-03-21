38c805ffd9a91d544c1825a3f2225dcd7345f5e8
johnoro <officialjohnorourke@gmail.com>
2019-03-21 13:00:07 -0400

    Adds organizations migration
    Also updates migration file naming convention to be more clear for our project

--- db/migrations/01_users.js
+++ db/migrations/01_users.js
@@ -3,16 +3,17 @@ const users = 'users';
 exports.up = knex =>
   knex.schema.createTable(users, table => {
     const orgId = 'organization_id';
+    table.increments();
     table.string('username').unique();
     table.string('first_name');
     table.string('last_name');
     table.string('password');
     table.string('role');
     table.integer(orgId).unsigned();
-    // table
-    //   .foreign(orgId)
-    //   .references('id')
-    //   .on('organizations');
+    table
+      .foreign(orgId)
+      .references('id')
+      .on('organizations');
   });
 
 exports.down = knex => knex.schema.dropTableIfExists(users);
