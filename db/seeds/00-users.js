acf9db9c3383c5bcdf2d6835d3eb1cd9d065742f
Grant Reighard <grant.reighard@gmail.com>
2019-03-21 18:12:10 -0400

    Updates users seed with working code.

--- db/seeds/00-users.js
+++ db/seeds/00-users.js
@@ -3,20 +3,20 @@ let fakeUsers = [];
 roles = ['teacher', 'board member', 'school admin'];
 
 for (let i = 0; i < 500; i++) {
-  const username = faker.fake(internet.userName);
-  const firstName = faker.fake(name.firstName);
-  const lastName = faker.fake(name.lastName);
-  const password = faker.fake(internet.password);
+  const username = faker.fake('{{internet.userName}}');
+  const first_name = faker.fake('{{name.firstName}}');
+  const last_name = faker.fake('{{name.lastName}}');
+  const password = faker.fake('{{internet.password}}');
   const role = roles[Math.floor(Math.random() * roles.length)];
-  const orgId = faker.fake(random.number);
+  const organization_id = faker.fake('{{random.number}}');
 
   const object = {
     username: username,
-    firstName: firstName,
-    lastName: lastName,
+    first_name: first_name,
+    last_name: last_name,
     password: password,
-    role: role,
-    orgId: orgId
+    role: role
+    //organization_id: organization_id
   };
 
   fakeUsers.push(object);
@@ -25,7 +25,7 @@ for (let i = 0; i < 500; i++) {
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
   return knex('users')
-    .del()
+    .truncate()
     .then(function() {
       // Inserts seed entries
       return knex('users').insert(fakeUsers);
