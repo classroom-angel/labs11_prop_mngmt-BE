5777851fa8176ab45deb40e00d7c2b5d1a4e1cb2
johnoro <officialjohnorourke@gmail.com>
2019-03-24 19:54:12 -0400

    Adds temporary placement of automatic index routing for the api

--- api/index.js
+++ api/index.js
@@ -2,13 +2,16 @@ const fs = require('fs');
 const path = require('path');
 const router = require('express').Router();
 
-fs.readdir(path.resolve(__dirname, migrations), (err, files) =>
-  files.forEach()
+fs.readdir(path.resolve(__dirname, './'), (err, files) =>
+  files.forEach(file => {
+    const theWay = `./${file}`;
+    router.use(theWay.slice(1), require(theWay));
+  })
 );
 
-router.use('/organizations', require('./organizations'));
-router.use('/users', require('./users'));
-router.use('/equipment', require('./equipment'));
-router.use('/issues', require('./issues'));
+// router.use('/organizations', require('./organizations'));
+// router.use('/users', require('./users'));
+// router.use('/equipment', require('./equipment'));
+// router.use('/issues', require('./issues'));
 
 module.exports = router;
