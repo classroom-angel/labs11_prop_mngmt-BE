5a63960dc38d9fabad39daa5d913603e19a4e728
Grant Reighard <grant.reighard@gmail.com>
2019-03-25 16:02:45 -0400

    Adds delete and update endpoints for issues.

--- api/issues/index.js
+++ api/issues/index.js
@@ -1,10 +1,19 @@
 const router = require('express').Router();
-const { create, read } = require('../../db/dataHelpers/issues');
+const {
+  create,
+  read,
+  update,
+  deleted
+} = require('../../db/dataHelpers/issues');
 
 router.use('/status', require('./status'));
 
 router.get('', read);
 
 router.post('', create);
+
+router.put('', update);
+
+router.delete('', deleted);
 
 module.exports = router;
