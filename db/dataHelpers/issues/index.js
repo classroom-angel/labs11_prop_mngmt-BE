1f07a9a9a93c2e201bab49f427af3a7ec213f0fe
Grant Reighard <grant.reighard@gmail.com>
2019-03-25 16:14:20 -0400

    Adds update and deleted to dataHelpers issues index.

--- db/dataHelpers/issues/index.js
+++ db/dataHelpers/issues/index.js
@@ -1,7 +1,7 @@
 const create = require('./create');
 const read = require('./read');
 const update = require('./update');
-const deleted = require('./delete');
+const deleted = require('./deleted');
 
 module.exports = {
   create,
