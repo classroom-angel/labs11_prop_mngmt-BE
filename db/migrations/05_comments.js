3129a8ad05a396e222ef9bf148939dd70c9386a8
johnoro <officialjohnorourke@gmail.com>
2019-03-25 14:01:13 -0400

    db/migrations/05_comments:

--- db/migrations/05_comments.js
+++ db/migrations/05_comments.js
@@ -3,8 +3,19 @@ const comments = 'comments';
 exports.up = knex =>
   knex.schema.createTable(comments, table => {
     const issueId = 'issue_id';
+    const userId = 'user_id';
     table.increments();
     table.string('content').notNullable();
+    // commenter
+    table
+      .integer(userId)
+      .unsigned()
+      .notNullable();
+    table
+      .foreign(userId)
+      .references('id')
+      .on('users')
+      .onDelete('CASCADE');
     table
       .integer(issueId)
       .unsigned()
