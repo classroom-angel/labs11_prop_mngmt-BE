c43d2798da7efb8d2ae3ec820efe5ae608263505
johnoro <officialjohnorourke@gmail.com>
2019-03-25 16:33:54 -0400

    db/dataHelpers/equipment/deleted: create

---
+++ db/dataHelpers/equipment/deleted.js
@@ -0,0 +1,23 @@
+const db = require('../../dbConfig');
+
+const deleted = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const deleteResponse = await db('equipment')
+      .where({ id })
+      .del();
+
+    if (deleteResponse) {
+      res.status(200).json({ deleteResponse });
+    } else {
+      res.status(400).json({
+        error: "You are trying to delete something that's not there."
+      });
+    }
+  } catch (error) {
+    res.status(500).json({ error });
+  }
+};
+
+module.exports = deleted;
