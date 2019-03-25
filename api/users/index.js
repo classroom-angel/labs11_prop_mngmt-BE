2f11c241e6bf1273e58167f881d09745ab2f00ae
Grant Reighard <grant.reighard@gmail.com>
2019-03-25 16:39:15 -0400

    Adds user delete and update endpoints.

--- api/users/index.js
+++ api/users/index.js
@@ -1,9 +1,11 @@
 const router = require('express').Router();
-const { read } = require('../../db/dataHelpers/users');
+const { read, deleted, update } = require('../../db/dataHelpers/users');
 
 router.use('/register', require('./register'));
 router.use('/login', require('./login'));
 
 router.get('', read);
+router.delete('', deleted);
+router.put('', update);
 
 module.exports = router;
--- db/dataHelpers/users/update.js
+++ db/dataHelpers/users/update.js
@@ -1,8 +1,40 @@
 const db = require('../../dbConfig');
+const {
+  helpers: { readByName }
+} = require('../organizations');
+const bcrypt = require('bcryptjs');
 
 const update = async (req, res) => {
   try {
-    // PUT usercode
+    const { id } = req.params;
+    const {
+      username,
+      firstName,
+      lastName,
+      password,
+      role,
+      organizationName
+    } = req.body;
+
+    const { org_id } = readByName(organizationName);
+    const hash = bcrypt.hashSync(password, Number(process.env.HASH_SALT) || 12);
+
+    const updateResponse = await db('users')
+      .where('id', id)
+      .update({
+        username,
+        first_name: firstName,
+        last_name: lastName,
+        password: hash,
+        role,
+        organization_id: org_id
+      });
+
+    if (updateResponse) {
+      res.status(200).json({ updateResponse });
+    } else {
+      res.status(400).json({ error: 'Could not update user in database.' });
+    }
   } catch (error) {
     res.status(500).json({ error });
   }
