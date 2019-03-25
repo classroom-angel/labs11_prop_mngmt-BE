0058fc1b8708bf3304f690ee471e5b92991a274b
johnoro <officialjohnorourke@gmail.com>
2019-03-24 20:50:44 -0400

    Restores old user creation; contains bug with null org id

--- db/dataHelpers/users/create.js
+++ db/dataHelpers/users/create.js
@@ -5,37 +5,37 @@ const {
 const bcrypt = require('bcryptjs');
 
 const register = async (req, res) => {
-  // try {
-  const {
-    username,
-    firstName,
-    lastName,
-    password,
-    role,
-    organizationName
-  } = req.body;
+  try {
+    const {
+      username,
+      firstName,
+      lastName,
+      password,
+      role,
+      organizationName
+    } = req.body;
 
-  const hash = bcrypt.hashSync(password, Number(process.env.HASH_SALT) || 12);
+    const hash = bcrypt.hashSync(password, Number(process.env.HASH_SALT) || 12);
 
-  const { id } = readByName(organizationName);
+    const { id } = readByName(organizationName);
 
-  const userResponse = await db('users').insert({
-    username,
-    first_name: firstName,
-    last_name: lastName,
-    password: hash,
-    role,
-    organization_id: id
-  });
+    const userResponse = await db('users').insert({
+      username,
+      first_name: firstName,
+      last_name: lastName,
+      password: hash,
+      role,
+      organization_id: id
+    });
 
-  if (userResponse) {
-    res.status(200).json({ userResponse });
-  } else {
-    res.status(400).json({ error: 'You probably did a bad with your data.' });
+    if (userResponse) {
+      res.status(200).json({ userResponse });
+    } else {
+      res.status(400).json({ error: 'You probably did a bad with your data.' });
+    }
+  } catch (error) {
+    res.status(500).json({ error });
   }
-  // } catch (error) {
-  //   res.status(500).json({ error });
-  // }
 };
 
 module.exports = register;
