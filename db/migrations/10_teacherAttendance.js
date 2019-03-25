47d733077a5763efa4cb65dd33a5e676a225d5d9
johnoro <officialjohnorourke@gmail.com>
2019-03-25 18:08:14 -0400

    Corrects foreign id usage in teacher attendance migration

--- db/migrations/10_teacherAttendance.js
+++ db/migrations/10_teacherAttendance.js
@@ -5,7 +5,7 @@ exports.up = knex =>
   knex.schema.createTable(teacherAttendance, table => {
     const userId = 'id';
     table.primary(userId);
-    foreignId(userId);
+    foreignId(table, userId, 'users');
   });
 
 exports.down = knex => knex.schema.dropTableIfExists(teacherAttendance);
