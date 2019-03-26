a94d5abb86c8785766f89b3b0204d40d8be8ce77
johnoro <officialjohnorourke@gmail.com>
2019-03-26 11:44:15 -0400

    Adds attendance return data

--- db/dataHelpers/attendance/deleted.js
+++ db/dataHelpers/attendance/deleted.js
@@ -4,12 +4,13 @@ const deleted = async (req, res) => {
   try {
     const { id } = req.params;
 
-    const deleteResponse = await db('teacher_attendance')
+    const [attendance] = await db('teacher_attendance')
       .where({ id })
-      .del();
+      .del()
+      .returning('*');
 
-    if (deleteResponse) {
-      res.status(200).json({ deleteResponse });
+    if (attendance) {
+      res.status(200).json({ attendance });
     } else {
       res.status(400).json({
         error: "You are trying to delete something that's not there."
