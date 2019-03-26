4f2dea60bf6504b2870c3acfaa335f59bac22483
johnoro <officialjohnorourke@gmail.com>
2019-03-26 10:48:18 -0400

    db/dataHelpers/tags/update:

--- db/dataHelpers/tags/update.js
+++ db/dataHelpers/tags/update.js
@@ -3,16 +3,26 @@ const db = require('../../dbConfig');
 const update = async (req, res) => {
   try {
     const { id } = req.params;
-    const { name } = req.body;
+    const { name, issueId } = req.body;
 
-    const updateResponse = await db('tags')
+    const [tag] = await db('tags')
       .where({ id })
       .update({
         name
-      });
+      })
+      .returning('*');
 
-    if (updateResponse) {
-      res.status(200).json({ updateResponse });
+    if (tag) {
+      if (issueId) {
+        var [issueJoinTag] = await db('issues_join_tags')
+          .where({ tag_id: id })
+          .update({
+            issue_id: issueId
+          })
+          .returning('*');
+      }
+
+      res.status(200).json({ tag, issueJoinTag });
     } else {
       res.status(400).json({ error: 'Could not update tag in database.' });
     }
