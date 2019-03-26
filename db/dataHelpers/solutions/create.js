e2f09b7640039d0865b565ddc53a191293b4f084
johnoro <officialjohnorourke@gmail.com>
2019-03-26 11:00:45 -0400

    db/dataHelpers/solutions/create:

--- db/dataHelpers/solutions/create.js
+++ db/dataHelpers/solutions/create.js
@@ -2,18 +2,29 @@ const db = require('../../dbConfig');
 
 const create = async (req, res) => {
   try {
-    const { date, time } = req.body;
+    const { date, time, issueId } = req.body;
 
-    const issueResponse = await db('solutions').insert({
-      date,
-      time
-    });
+    const [solution] = await db('solutions')
+      .insert({
+        date,
+        time
+      })
+      .returning('*');
 
-    if (issueResponse) {
-      res.status(200).json({ issueResponse });
+    if (solution) {
+      if (issueId) {
+        var [issueJoinSolution] = await db('issues_join_solutions')
+          .insert({
+            issue_id: issueId,
+            solution_id: solution.id
+          })
+          .returning('*');
+      }
     } else {
       res.status(400).json({ error: 'You probably did a bad with your data.' });
     }
+
+    res.status(200).json({ solution, issueJoinSolution });
   } catch (error) {
     res.status(500).json({ error });
   }
