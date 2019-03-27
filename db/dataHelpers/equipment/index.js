98f37ff732de5725aad459c65f59c1a1cbb3ddc4
johnoro <officialjohnorourke@gmail.com>
2019-03-27 11:42:57 -0400

    Quickfix imports with missing slash

--- db/dataHelpers/equipment/index.js
+++ db/dataHelpers/equipment/index.js
@@ -1,6 +1,6 @@
 const create = require('./create');
 const read = require('./read');
-const readById = require('.readById');
+const readById = require('./readById');
 const update = require('./update');
 const deleted = require('./deleted');
 
