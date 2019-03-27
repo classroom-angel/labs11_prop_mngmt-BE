4930c7ad4e44a0bc5cb41ec7c8032543450228b2
johnoro <officialjohnorourke@gmail.com>
2019-03-27 15:20:07 -0400

    Ties issues to tags where was previously missing

--- db/dataHelpers/tags/read.js
+++ db/dataHelpers/tags/read.js
@@ -1,11 +1,13 @@
 const db = require('../../dbConfig');
 const { keysToCamelCase } = require('../');
+const joinIssue = require('./joinIssue');
 
 const read = async (req, res) => {
   try {
     const tags = await db('tags');
     if (tags.length) {
       tags = tags.map(tag => keysToCamelCase(tag));
+      tags = tags.map(tag => joinIssue(tag));
       res.status(200).json({ tags });
     } else {
       res.status(404).json({ error: 'No tags were found.' });
--- db/dataHelpers/tags/readById.js
+++ db/dataHelpers/tags/readById.js
@@ -1,5 +1,6 @@
 const db = require('../../dbConfig');
 const { keysToCamelCase } = require('../');
+const joinIssue = require('./joinIssue');
 
 const readById = async (req, res) => {
   try {
@@ -9,6 +10,7 @@ const readById = async (req, res) => {
       .first();
     if (tag) {
       tag = keysToCamelCase(tag);
+      tag = joinIssue(tag);
       res.status(200).json({ tag });
     } else {
       res.status(404).json({ error: 'No tag was found.' });
