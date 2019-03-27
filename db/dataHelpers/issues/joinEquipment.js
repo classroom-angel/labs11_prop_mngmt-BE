ded7e88a9089dcb90b4e8d89383e413c4de84f10
johnoro <officialjohnorourke@gmail.com>
2019-03-27 14:52:23 -0400

    Fixes lack of async in helper function

--- db/dataHelpers/issues/joinEquipment.js
+++ db/dataHelpers/issues/joinEquipment.js
@@ -1,14 +1,16 @@
-const joinEquipment = issue => {
+const joinEquipment = async issue => {
   const [equipmentJoinIssue] = await db('equipment_join_issues')
     .where({
       issue_id: issue.id
     })
     .returning('*');
   if (equipmentJoinIssue) {
-    const [equipment] = await db('equipment').where({
-      id: equipmentJoinIssue.equipment_id
-    }).returning('*');
-    
+    const [equipment] = await db('equipment')
+      .where({
+        id: equipmentJoinIssue.equipment_id
+      })
+      .returning('*');
+
     return { ...issue, equipmentId: equipment.id };
   }
 
