7419b2f68eb1e6990c79175bddc7efe80503a2cf
johnoro <officialjohnorourke@gmail.com>
2019-03-28 12:50:06 -0400

    test/users.spec:

--- test/users.spec.js
+++ test/users.spec.js
@@ -3,6 +3,7 @@ const server = require('../index');
 const expect = require('chai').expect;
 
 describe('Unit tests for users', () => {
+  const route = '/api/users';
   const userFields = [
     'id',
     'username',
@@ -13,20 +14,20 @@ describe('Unit tests for users', () => {
   ];
 
   it('has required keys on users GET route', async () => {
-    const response = await request(server).get('/api/users/');
+    const response = await request(server).get(route);
 
     expect(response.body.users[0]).to.have.keys(...userFields);
   });
 
   it('has required keys on users GET ID route', async () => {
-    const response = await request(server).get('/api/users/1');
+    const response = await request(server).get(`${route}/1`);
 
     expect(response.body.user).to.have.keys(...userFields);
   });
 
   it('has required keys on users register POST route', async () => {
     const response = await request(server)
-      .post('/api/users/register')
+      .post(`${route}/register`)
       .send({
         username: 'greg.davis@gmail.com',
         firstName: 'Greg',
