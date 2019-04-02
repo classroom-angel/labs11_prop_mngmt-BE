bda6b0e6a9883b70734dd36a43850836f409d485
johnoro <officialjohnorourke@gmail.com>
2019-04-02 13:01:13 -0400

    Adds camelCased return for images

--- db/dataHelpers/images/index.js
+++ db/dataHelpers/images/index.js
@@ -1,4 +1,5 @@
 const db = require('../../dbConfig');
+const { keysToCamelCase } = require('../');
 
 const create = async ({ paths, issueId }) => {
   try {
@@ -6,7 +7,7 @@ const create = async ({ paths, issueId }) => {
       .insert(paths.map(path => ({ path, issue_id: issueId })))
       .returning('*');
 
-    if (images.length) return images;
+    if (images.length) return keysToCamelCase(images);
     else return null;
   } catch (err) {
     return err;
