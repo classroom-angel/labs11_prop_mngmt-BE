a677804d302492b57e2270b3fef1bb7867a89b98
johnoro <officialjohnorourke@gmail.com>
2019-04-02 12:57:01 -0400

    Adds image POST

---
+++ db/dataHelpers/images/index.js
@@ -0,0 +1,18 @@
+const db = require('../../dbConfig');
+
+const create = async ({ paths, issueId }) => {
+  try {
+    const images = await db('images')
+      .insert(paths.map(path => ({ path, issue_id: issueId })))
+      .returning('*');
+
+    if (images.length) return images;
+    else return null;
+  } catch (err) {
+    return err;
+  }
+};
+
+module.exports = {
+  create
+};
