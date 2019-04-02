7ea26566e123e8ea42c464fd18a207a035f45f9d
johnoro <officialjohnorourke@gmail.com>
2019-04-02 12:57:14 -0400

    Cleans up image upload

--- api/upload/index.js
+++ api/upload/index.js
@@ -1,11 +1,22 @@
 const router = require('express').Router();
 const formData = require('express-form-data');
 const cloudinary = require('cloudinary').v2;
+const { create } = require('../../db/dataHelpers/images');
 
-router.post('', formData.parse(), ({ files }, res) => {
+router.post('', formData.parse(), ({ files, body: { issueId } }, res) => {
   const images = Object.values(files);
   const results = images.map(image => cloudinary.uploader.upload(image.path));
-  Promise.all(results).then(result => res.json(result));
+  Promise.all(results).then(async cloud => {
+    try {
+      const paths = cloud.map(
+        ({ public_id, format }) => `${public_id}.${format}`
+      );
+      const local = await create({ paths, issueId });
+      res.json({ cloud, local });
+    } catch (error) {
+      res.status(500).json({ error });
+    }
+  });
 });
 
 module.exports = router;
