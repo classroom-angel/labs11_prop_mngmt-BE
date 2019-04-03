ac079d2111817d48139308b8c044c90d8aeba605
Rachel DiCesare <racheldicesare93@gmail.com>
2019-04-03 18:58:38 -0400

    created testimonial data for out testimonials page for the frontend

---
+++ db/dataHelpers/testimonials/create.js
@@ -0,0 +1,43 @@
+const db = require('../../dbConfig');
+const {
+    keysToCamelCase
+} = require('../');
+
+const create = async (req, res) => {
+    const {
+        name,
+        role,
+        text
+    } = req.body;
+    try {
+        let [testimonials] = await db('testimonials')
+            .insert({
+                name: name,
+                role: role,
+                text: text
+            })
+            .returning('*');
+        if (testimonials) {
+            testimonials = keysToCamelCase(testimonials);
+            res.status(200).json({
+                testimonials
+            });
+        } else {
+            res.status(400).json({
+                error: 'Cannot add testimonial.'
+            });
+        }
+    } catch (error) {
+        if (!name || !role || !text) {
+            res.status(422).json({
+                error: 'Required body information: name , role, text.'
+            });
+        } else {
+            res.status(500).json({
+                error
+            });
+        }
+    }
+};
+
+module.exports = create;
