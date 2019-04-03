a5f0e3f43e1bddd464616d71da9e101ec7137f12
Grant Reighard <grant.reighard@gmail.com>
2019-04-03 12:42:21 -0400

    Adds Stripe Connect.

--- api/payment/index.js
+++ api/payment/index.js
@@ -12,4 +12,8 @@ router.post('', (req, res) => {
   stripe.charges.create(req.body, postCharge(res));
 });
 
+router.post('/connect', (req, res) => {
+  stripe.charges.create(req.body, postCharge(res));
+});
+
 module.exports = router;
