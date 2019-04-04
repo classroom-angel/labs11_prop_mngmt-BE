9b5b40ced71b1d01cf9aa2d6af0d9516dda77fb1
johnoro <officialjohnorourke@gmail.com>
2019-04-04 16:09:34 -0400

    Adds connect route logic

---
+++ api/payment/connect.js
@@ -0,0 +1,31 @@
+const router = require('express').Router();
+const request = require('request');
+
+router.post('', ({ body: { code } }, res) => {
+  const data = {
+    client_secret: process.env.STRIPE_DEV_KEY,
+    code,
+    grant_type: 'authorization_code'
+  };
+
+  const options = {
+    url: 'https://connect.stripe.com/oauth/token',
+    method: 'POST',
+    body: data
+  };
+
+  const handleData = (error, response, body) => {
+    if (!error && response.statusCode == 200) {
+      console.log(body);
+      const { refresh_token, stripe_user_id } = body;
+      res.json({
+        refresh_token,
+        stripe_user_id
+      });
+    } else res.status(500).json({ error });
+  };
+
+  request(options, handleData);
+});
+
+module.exports = router;
