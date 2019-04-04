2ee9470646aa2eb431efff53e145e70be3e6303a
johnoro <officialjohnorourke@gmail.com>
2019-04-04 16:40:22 -0400

    Updates the way connect data is sent to stripe token api

--- api/payment/connect.js
+++ api/payment/connect.js
@@ -2,11 +2,10 @@ const router = require('express').Router();
 const request = require('request');
 
 router.post('', ({ body: { code } }, res) => {
-  const data = {
-    client_secret: process.env.STRIPE_DEV_KEY,
-    code,
-    grant_type: 'authorization_code'
-  };
+  const data =
+    `client_secret=${process.env.STRIPE_DEV_KEY}&` +
+    `code=${code}&` +
+    'grant_type=authorization_code';
 
   const options = {
     url: 'https://connect.stripe.com/oauth/token',
@@ -16,7 +15,6 @@ router.post('', ({ body: { code } }, res) => {
 
   const handleData = (error, response, body) => {
     if (!error && response.statusCode == 200) {
-      console.log(body);
       const { refresh_token, stripe_user_id } = body;
       res.json({
         refresh_token,
