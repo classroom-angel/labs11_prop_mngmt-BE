399abd304a44e97a72b32d32ac840d88290813a2
johnoro <officialjohnorourke@gmail.com>
2019-04-04 19:37:33 -0400

    Fixes merge conflict
    I forgot to save before pushing, oops!

--- db/seeds/04_issues.js
+++ db/seeds/04_issues.js
@@ -1,6 +1,5 @@
 const fakeIssues = [];
 
-<<<<<<< HEAD
 fakeIssues.push(
   {
     name: 'Karen visit',
@@ -49,48 +48,6 @@ fakeIssues.push(
     organization_id: 2
   }
 );
-=======
-fakeIssues.push({
-  name: 'Karen visit',
-  notes: 'Clean up!',
-  is_visit: true,
-  organization_id: 1
-}, {
-  name: 'Need more books',
-  notes: 'Will order',
-  is_visit: true,
-  status: 'scheduled',
-  organization_id: 1
-}, {
-  name: 'Desk leg is broken',
-  notes: 'It is all wobbly and stuff',
-  is_visit: true,
-  status: 'ignored',
-  organization_id: 1
-}, {
-  name: 'Macbook has red dot on screen',
-  notes: 'Will handle in summer',
-  is_visit: true,
-  status: 'ignored',
-  organization_id: 1
-}, {
-  name: 'It will not turn on',
-  notes: 'Will handle in summer',
-  is_visit: true,
-  status: 'ignored',
-  organization_id: 1
-}, {
-  name: 'Cafeteria missing a knife',
-  notes: 'Will handle in summer',
-  is_visit: true,
-  status: 'ignored',
-  organization_id: 1
-}, {
-  name: 'Page missing from this book',
-  notes: 'There are so many!',
-  organization_id: 2
-});
->>>>>>> df5deb3ed44d2b7b53ba0c74f14467f492df39c9
 
 const { makeSeed } = require('../dataHelpers/seeds');
 exports.seed = makeSeed('issues', fakeIssues);
