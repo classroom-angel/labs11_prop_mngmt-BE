35f30ab763a0ae965c40cbbf3bbe67b06a97cdf1
Grant Reighard <grant.reighard@gmail.com>
2019-04-04 17:11:17 -0400

    Adds conditional to createImages.js.

--- db/dataHelpers/issues/createImages.js
+++ db/dataHelpers/issues/createImages.js
@@ -4,18 +4,20 @@ const { create } = require('../images');
 const createImages = ({ files, params: { id: issueId } }, res) => {
   let [images] = Object.values(files);
   if (!Array.isArray(images)) images = [images];
-  const results = images.map(image => cloudinary.uploader.upload(image.path));
-  Promise.all(results).then(async cloud => {
-    try {
-      const paths = cloud.map(
-        ({ public_id, format }) => `${public_id}.${format}`
-      );
-      const local = await create({ paths, issueId });
-      res.json({ cloud, local });
-    } catch (error) {
-      res.status(500).json({ error });
-    }
-  });
+  if (images) {
+    const results = images.map(image => cloudinary.uploader.upload(image.path));
+    Promise.all(results).then(async cloud => {
+      try {
+        const paths = cloud.map(
+          ({ public_id, format }) => `${public_id}.${format}`
+        );
+        const local = await create({ paths, issueId });
+        res.json({ cloud, local });
+      } catch (error) {
+        res.status(500).json({ error });
+      }
+    });
+  }
 };
 
 module.exports = createImages;
