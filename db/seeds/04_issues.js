fcb2b93446346535a1f4783865ec0d4e538cf95b
johnoro <officialjohnorourke@gmail.com>
2019-04-04 19:33:51 -0400

    Removes seeded empty issues

--- db/seeds/04_issues.js
+++ db/seeds/04_issues.js
@@ -1,47 +1,53 @@
 const fakeIssues = [];
 
-fakeIssues.push({
-  name: 'Karen visit',
-  notes: 'Clean up!',
-  is_visit: true,
-  organization_id: 1
-}, {
-  name: 'Need more books',
-  notes: 'Will order',
-  is_visit: true,
-  status: 'scheduled',
-  organization_id: 1
-}, {
-  name: 'Desk leg is broken',
-  notes: 'It is all wobbly and stuff',
-  is_visit: true,
-  status: 'ignored',
-  organization_id: 1
-}, {}, {
-  name: 'Macbook has red dot on screen',
-  notes: 'Will handle in summer',
-  is_visit: true,
-  status: 'ignored',
-  organization_id: 1
-}, {}, {
-  name: 'It will not turn on',
-  notes: 'Will handle in summer',
-  is_visit: true,
-  status: 'ignored',
-  organization_id: 1
-}, {}, {
-  name: 'Cafeteria missing a knife',
-  notes: 'Will handle in summer',
-  is_visit: true,
-  status: 'ignored',
-  organization_id: 1
-}, {
-  name: 'Page missing from this book',
-  notes: 'There are so many!',
-  organization_id: 2
-});
+fakeIssues.push(
+  {
+    name: 'Karen visit',
+    notes: 'Clean up!',
+    is_visit: true,
+    organization_id: 1
+  },
+  {
+    name: 'Need more books',
+    notes: 'Will order',
+    is_visit: true,
+    status: 'scheduled',
+    organization_id: 1
+  },
+  {
+    name: 'Desk leg is broken',
+    notes: 'It is all wobbly and stuff',
+    is_visit: true,
+    status: 'ignored',
+    organization_id: 1
+  },
+  {
+    name: 'Macbook has red dot on screen',
+    notes: 'Will handle in summer',
+    is_visit: true,
+    status: 'ignored',
+    organization_id: 1
+  },
+  {
+    name: 'It will not turn on',
+    notes: 'Will handle in summer',
+    is_visit: true,
+    status: 'ignored',
+    organization_id: 1
+  },
+  {
+    name: 'Cafeteria missing a knife',
+    notes: 'Will handle in summer',
+    is_visit: true,
+    status: 'ignored',
+    organization_id: 1
+  },
+  {
+    name: 'Page missing from this book',
+    notes: 'There are so many!',
+    organization_id: 2
+  }
+);
 
-const {
-  makeSeed
-} = require('../dataHelpers/seeds');
+const { makeSeed } = require('../dataHelpers/seeds');
 exports.seed = makeSeed('issues', fakeIssues);
