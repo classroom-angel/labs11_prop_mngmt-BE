b45398112b4366562794786bd5d27984743c49d7
johnoro <officialjohnorourke@gmail.com>
2019-04-08 16:21:52 -0400

    Formatting fix

--- db/migrations/12_testimonials.js
+++ db/migrations/12_testimonials.js
@@ -1,12 +1,11 @@
 const testimonials = 'testimonials';
 
 exports.up = knex =>
-    knex.schema.createTable(testimonials, table => {
-        table.increments();
-        table.string('name');
-        table.string('role');
-        table.string('text');
-
-    });
+  knex.schema.createTable(testimonials, table => {
+    table.increments();
+    table.string('name');
+    table.string('role');
+    table.string('text');
+  });
 
 exports.down = knex => knex.schema.dropTableIfExists(testimonials);
