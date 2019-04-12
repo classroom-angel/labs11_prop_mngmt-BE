90c9d8ef0560b9841e51f8b2311c82cd48601e66
johnoro <officialjohnorourke@gmail.com>
2019-04-12 12:12:17 -0400

    Removes sanity get route to test docs

--- index.js
+++ index.js
@@ -20,10 +20,6 @@ server.use(cors(corsOptions));
 server.use(express.json());
 server.use('/api', require('./api'));
 
-server.get('/', (req, res) => {
-  res.status(200).json({ success: "You're not insane!" });
-});
-
 const PORT = process.env.PORT || 8000;
 server.listen(PORT, () => console.log(`Server listening on port ${PORT}.`));
 
