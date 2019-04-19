bd45abaed9b7c8646a9769c9cb6c29f3157b81e7
Rachel DiCesare <racheldicesare93@gmail.com>
2019-04-19 13:03:14 -0400

    changed org name

--- db/seeds/01_organizations.js
+++ db/seeds/01_organizations.js
@@ -1,19 +1,18 @@
 const fakeOrganizations = [];
 
-fakeOrganizations.push(
-  {
-    name: 'Evil Corp',
-    city: 'NYC',
-    country: 'USA',
-    expected_hours: 100
-  },
-  {
-    name: 'Sunshine Corp',
-    city: 'Sunnyville',
-    country: 'USA',
-    expected_hours: 40
-  }
-);
+fakeOrganizations.push({
+  name: 'Rosehill All Girls Boarding School',
+  city: 'NYC',
+  country: 'USA',
+  expected_hours: 100
+}, {
+  name: 'Sunshine Corp',
+  city: 'Sunnyville',
+  country: 'USA',
+  expected_hours: 40
+});
 
-const { makeSeed } = require('../dataHelpers/seeds');
+const {
+  makeSeed
+} = require('../dataHelpers/seeds');
 exports.seed = makeSeed('organizations', fakeOrganizations);
